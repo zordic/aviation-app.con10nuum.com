0669efcaf2b8b4fe58cafd3fb4324071ab32004c
zordic <joar.donceras@gmail.com>
2025-04-18 17:42:22 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -273,5 +273,6 @@
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
     </body>
 </html>
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInit88970a0117c062eed55fa8728fc43833
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit88970a0117c062eed55fa8728fc43833::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInit88970a0117c062eed55fa8728fc43833::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire88970a0117c062eed55fa8728fc43833($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire88970a0117c062eed55fa8728fc43833($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
